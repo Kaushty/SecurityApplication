2e7bead02cf519932cdeada03efc0dd978aef4a7
kaushty <kaushikshetty4898@gmail.com>
2019-12-11 10:19:08 +0530

    SOS_Activity functionality completed

--- app/src/main/java/com/example/securityapplication/sos_page.java
+++ app/src/main/java/com/example/securityapplication/sos_page.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
